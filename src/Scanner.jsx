32d875e70e0a1d03ae8be0f967334c97c10d3a80
genie <banggijin@gameper.io>
2023-09-04 16:46:27 +0900

    can change camera

--- src/Scanner.jsx
+++ src/Scanner.jsx
@@ -42,12 +42,21 @@ const readText = (ourText) => {
 };
 
 let qrScanner = null;
+let currentCameraIndex = 0;
 export default function Scanner() {
   const [data, setData] = useState("고객");
   const [usage, setUsage] = useState("0");
   const [scannerSwitch, setScannerSwitch] = useState(0);
+  const [cameras, setCameras] = useState([]);
   useEffect(() => {
+    const setc = async () => {
+      const list = await QrScanner.listCameras(true);
+      setCameras(list);
+      console.log(list);
+    };
+    setc();
     const videoElem = document.getElementById("qrvideo");
+
     var device_height = 500;
     var device_width = 500;
     if (window.screen.height < window.screen.width) {
@@ -93,7 +102,18 @@ export default function Scanner() {
     }
   }, [scannerSwitch]);
   return (
-    <div>
+    <div
+      onClick={async () => {
+        if (cameras.length > 0) {
+          let _nextIndex = currentCameraIndex + 1;
+          if (_nextIndex === cameras.length) {
+            _nextIndex = 0;
+          }
+          currentCameraIndex = _nextIndex;
+          await qrScanner.setCamera(cameras[_nextIndex].id);
+        }
+      }}
+    >
       <div id="qrvideo-container" class="center-container">
         <video id="qrvideo"></video>
       </div>
